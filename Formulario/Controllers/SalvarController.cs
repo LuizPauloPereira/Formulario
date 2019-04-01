8a86653e860f9faaa4fa09e08b8537050a1b0e6e
Luiz Paulo Pereira <luizpaulopereiraf@gmail.com>
2019-04-01 15:54:05 -0400

    Adicionar arquivos de projeto.

---
+++ Formulario/Controllers/SalvarController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Formulario.Controllers
+{
+    public class SalvarController : Controller
+    {
+        // GET: Salvar
+        public ActionResult Index(string dados)
+        {
+            ViewBag.Dados = dados;
+            return View("Formulario");
+        }
+    }
+}
